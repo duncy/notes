66ce1457fd3c2dbcc8d9a7d4609df7d78269ebd9
Duncan Fryer <35885610+alt1f923@users.noreply.github.com>
2019-12-31 22:04:01 +1300

    completed parity with old learnpython site

--- docs/python/scripts/nav-chapter-one.js
+++ docs/python/scripts/nav-chapter-one.js
@@ -7,7 +7,10 @@ var navChapterOneData = {
     navList: [
         { id: 0, text: '1.1 - Getting Started', url: "https://tutor.xn--80ahslc6a.com/python" },
         { id: 1, text: '1.11 - Installation', url: "https://tutor.xn--80ahslc6a.com/python#Installation"},
-        { id: 2, text: '1.12 - Editors and Linters', url: "https://tutor.xn--80ahslc6a.com/python#Editors-and-Linters"}
+        { id: 2, text: '1.12 - Editors and Linters', url: "https://tutor.xn--80ahslc6a.com/python#Editors-and-Linters"},
+        { id: 3, text: '1.2 - What is Python', url: "https://tutor.xn--80ahslc6a.com/python/what-is-python.html"},
+        { id: 4, text: '1.21 - Python the Interpreter', url: "https://tutor.xn--80ahslc6a.com/python/what-is-python.html#Python-the-Interpreter"},
+        { id: 4, text: '1.22 - The Python shell', url: "https://tutor.xn--80ahslc6a.com/python/what-is-python.html#The-Python-shell"}
     ]
 }
 
