f6dcb6c6d3d6100f4296cac752b9fd78b1f0f42f
Duncan Fryer <35885610+alt1f923@users.noreply.github.com>
2020-01-01 15:28:48 +1300

    first pep mention woo
    cleant up code blocks, changed words, fixed css, added variables and expressions (not in that order) to the site

--- docs/python/scripts/nav-chapter-one.js
+++ docs/python/scripts/nav-chapter-one.js
@@ -10,7 +10,9 @@ var navChapterOneData = {
         { id: 2, text: '1.12 - Editors and Linters', url: "https://tutor.xn--80ahslc6a.com/python#Editors-and-Linters"},
         { id: 3, text: '1.2 - What is Python', url: "https://tutor.xn--80ahslc6a.com/python/what-is-python.html"},
         { id: 4, text: '1.21 - Python the Interpreter', url: "https://tutor.xn--80ahslc6a.com/python/what-is-python.html#Python-the-Interpreter"},
-        { id: 4, text: '1.22 - The Python shell', url: "https://tutor.xn--80ahslc6a.com/python/what-is-python.html#The-Python-shell"}
+        { id: 5, text: '1.22 - The Python shell', url: "https://tutor.xn--80ahslc6a.com/python/what-is-python.html#The-Python-shell"},
+        { id: 6, text: '1.3 - Expressions', url: "https://tutor.xn--80ahslc6a.com/python/expressions.html"},
+        { id: 7, text: '1.4 - Variables', url: "https://tutor.xn--80ahslc6a.com/python/variables.html"}
     ]
 }
 
